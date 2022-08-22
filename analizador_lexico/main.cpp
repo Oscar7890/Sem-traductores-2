ae64605fd7a97986508882a354d151d72c4ffd09
Oscar7890 <73807285+Oscar7890@users.noreply.github.com>
2022-08-21 23:02:09 -0500

    Analizador sintactico
    Aun le falta completar el alfabeto de lenguaje

---
+++ analizador_lexico/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "lexico.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+
+    int a=0;
+    Lexico lexico;
+
+    //lexico.entrada("b aa33.33 1556 275.12 Hola int char float void string class bool +- */ <=>>=< || && ! !=== ; ,"
+    //               "() {} = if else while return .632");
+
+
+    while(a==0){
+        string palabra;
+        cout<< "\nEscribe algo: ";
+        getline(cin,palabra);
+        lexico.entrada(palabra);
+        cout<<palabra<<endl;
+
+        cout << "\nResultado del Analisis Lexico" << endl << endl;
+        cout << "Simbolo\t\tTipo" << endl;
+
+        while ( lexico.simbolo.compare("$") != 0 ){
+              lexico.sigSimbolo();
+              cout <<  lexico.simbolo << "\t\t" << lexico.tipoAcad(lexico.tipo) << endl;
+        }
+
+        //cin.get();
+
+    }
+
+    return 0;
+}
