8434e1fe419c1adadddde0fbea2a24250c55556d
Jorge Herrada <jorge.herrada.28@gmail.com>
2022-09-02 23:59:15 -0500

    .

--- analizador_lexico/main.cpp
+++ analizador_lexico/main.cpp
@@ -6,32 +6,34 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
+    int a = 0;
+    // lexico.entrada("b aa33.33 1556 275.12 Hola int char float void string class bool +- */ <=>>=< || && ! !=== ; ,"
+    //                "() {} = if else while return .632");
 
-    int a=0;
-    //lexico.entrada("b aa33.33 1556 275.12 Hola int char float void string class bool +- */ <=>>=< || && ! !=== ; ,"
-    //               "() {} = if else while return .632");
+    while (a == 0)
+    {
 
-
-    while(a==0){
         Lexico lexico;
         string palabra;
-        cout<< "\nEscribe algo: ";
-        getline(cin,palabra);
-        lexico.entrada(palabra);
-        cout<<palabra<<endl;
+        cout << "\nEscribe algo: ";
+        getline(cin, palabra);
+        lexico.entrada(palabra); // Guarda palabra en variable interna "fuente"
+        cout << palabra << endl;
 
-        cout << "\nResultado del Analisis Lexico" << endl << endl;
+        cout << "\nResultado del Analisis Lexico" << endl
+             << endl;
         cout << "Simbolo\t\tTipo" << endl;
 
-        while ( lexico.simbolo.compare("$") != 0 ){
-              lexico.sigSimbolo();
-              cout <<  lexico.simbolo << "\t\t" << lexico.tipoAcad(lexico.tipo) << endl;
+        while (lexico.simbolo.compare("$") != 0)
+        {
+            lexico.sigSimbolo();
+            cout << lexico.simbolo << "\t\t" << lexico.tipoAcad(lexico.tipo) << endl;
         }
 
-        //cin.get();
-
+        // cin.get();
     }
 
     return 0;
